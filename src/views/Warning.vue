45b7404d7f252d9f2adb37d6e745e39918b9accd
Bagus M Kusuma <bmkusuma17@gmail.com>
2025-03-14 16:43:13 +0700

    Refine Strucute in app.vue

---
+++ src/views/Warning.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-gray-100 text-center">
+    <div>
+      <h1 class="text-2xl font-bold text-gray-800">Silakan buka di perangkat mobile 📱</h1>
+      <p class="text-gray-600">Undangan ini hanya dapat diakses melalui perangkat seluler.</p>
+    </div>
+  </div>
+</template>
