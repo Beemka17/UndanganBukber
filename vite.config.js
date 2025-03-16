5eb40f25914fe036c435809066cca6b6d2d8415b
Bagus M Kusuma <bmkusuma17@gmail.com>
2025-03-16 16:21:50 +0700

    config vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import compression from 'vite-plugin-compression'
+
+const plugins = [vue(), vueJsx(), tailwindcss(), compression()]
+if (process.env.NODE_ENV === 'development') {
+  import('vite-plugin-vue-devtools').then((module) => {
+    plugins.push(module.default())
+  })
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+  base: '/',
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    minify: 'esbuild',
   },
 })
